1c1778c43e7c2dbc135e0e62197e15874a769664
Adeline Kropf <115115894+AdelineKropf@users.noreply.github.com>
2026-04-20 19:21:06 +0000

    Worked on adding classes to my pug files to enable more specific CSS, and fixed the index.js formatting for easier reading.

--- app.js
+++ app.js
@@ -12,6 +12,7 @@ const app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
+app.locals.title = "Downtown Donuts";
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
--- routes/index.js
+++ routes/index.js
@@ -2,77 +2,91 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next){
+router.get('/', function (req, res, next) {
   try {
-      req.db.query('SELECT * FROM comments;', (err, results) => {
-            if (err) {
-                    console.error('Error fetching comments:', err);
-                            return res.status(500).send('Error fetching comments');
-                                  }
-                                        /* Upon loading the app displays the landing page */
-                                              res.render('landing_page', { title: 'Downtown Donuts', comments: results });
-                                                  });
-                                                    } catch (error) {
-                                                        console.error('Error fetching items:', error);
-                                                            res.status(500).send('Error fetching items');
-                                                              }
-                                                              });
+    req.db.query('SELECT * FROM comments;', (err, results) => {
+      if (err) {
+        console.error('Error fetching comments:', err);
+        return res.status(500).send('Error fetching comments');
+      }
 
-                                                              router.post('/create', function (req, res, next) {
-                                                                  const { comment } = req.body;
-                                                                      /* Doesn't allow an empty comment to be submitted */ 
-                                                                          if (!comment.trim()) {
-                                                                                return res.status(400).json({ error: "Comment cannot be empty" });
-                                                                                    }
-                                                                                        try {
-                                                                                              req.db.query('INSERT INTO comments (comment) VALUES (?);', [comment], (err, results) => {
-                                                                                                      if (err) {
-                                                                                                                console.error('Error adding comment:', err);
-                                                                                                                          return res.status(500).send('Error adding comment');
-                                                                                                                                  }
-                                                                                                                                          console.log('Comment added successfully:', results);
-                                                                                                                                                  // Redirect to the home page after adding
-                                                                                                                                                          res.redirect('/');
-                                                                                                                                                                });
-                                                                                                                                                                    } catch (error) {
-                                                                                                                                                                          console.error('Error adding comment:', error);
-                                                                                                                                                                                res.status(500).send('Error adding comment');
-                                                                                                                                                                                    }
-                                                                                                                                                                                    });
+      // Upon loading the app displays the landing page
+      res.render('landing_page');
+    });
+  } catch (error) {
+    console.error('Error fetching items:', error);
+    res.status(500).send('Error fetching items');
+  }
+});
 
-                                                                                                                                                                                    router.post('/delete', function (req, res, next) {
-                                                                                                                                                                                        const { id } = req.body;
-                                                                                                                                                                                            try {
-                                                                                                                                                                                                  req.db.query('DELETE FROM comments WHERE id = ?;', [id], (err, results) => {
-                                                                                                                                                                                                          if (err) {
-                                                                                                                                                                                                                    console.error('Error deleting comment:', err);
-                                                                                                                                                                                                                              return res.status(500).send('Error deleting comment');
-                                                                                                                                                                                                                                      }
-                                                                                                                                                                                                                                              console.log('Comment deleted successfully:', results);
-                                                                                                                                                                                                                                                      // Redirect to the home page after deletion
-                                                                                                                                                                                                                                                              res.redirect('/');
-                                                                                                                                                                                                                                                                  });
-                                                                                                                                                                                                                                                                      }catch (error) {
-                                                                                                                                                                                                                                                                              console.error('Error deleting comment:', error);
-                                                                                                                                                                                                                                                                                      res.status(500).send('Error deleting comment:');
-                                                                                                                                                                                                                                                                                          }
-                                                                                                                                                                                                                                                                                          });
+/* CREATE comment */
+router.post('/create', function (req, res, next) {
+  const { comment } = req.body;
 
-                                                                                                                                                                                                                                                                                          router.get('/menu', (req, res) => {
-                                                                                                                                                                                                                                                                                            res.render('menu');
-                                                                                                                                                                                                                                                                                            });
+  // Doesn't allow an empty comment to be submitted
+  if (!comment.trim()) {
+    return res.status(400).json({ error: "Comment cannot be empty" });
+  }
 
-                                                                                                                                                                                                                                                                                            router.get('/about_us', (req, res) => {
-                                                                                                                                                                                                                                                                                              res.render('about_us');
-                                                                                                                                                                                                                                                                                              });
+  try {
+    req.db.query(
+      'INSERT INTO comments (comment) VALUES (?);',
+      [comment],
+      (err, results) => {
+        if (err) {
+          console.error('Error adding comment:', err);
+          return res.status(500).send('Error adding comment');
+        }
 
-                                                                                                                                                                                                                                                                                              router.get('/customer_comments', (req, res) => {
-                                                                                                                                                                                                                                                                                                res.render('customer_comments');
-                                                                                                                                                                                                                                                                                                });
+        console.log('Comment added successfully:', results);
+        res.redirect('/');
+      }
+    );
+  } catch (error) {
+    console.error('Error adding comment:', error);
+    res.status(500).send('Error adding comment');
+  }
+});
 
-                                                                                                                                                                                                                                                                                                router.get('/landing_page', (req, res) => {
-                                                                                                                                                                                                                                                                                                  res.render('landing_page');
-                                                                                                                                                                                                                                                                                                  });
+/* DELETE comment */
+router.post('/delete', function (req, res, next) {
+  const { id } = req.body;
 
+  try {
+    req.db.query(
+      'DELETE FROM comments WHERE id = ?;',
+      [id],
+      (err, results) => {
+        if (err) {
+          console.error('Error deleting comment:', err);
+          return res.status(500).send('Error deleting comment');
+        }
 
-                                                                                                                                                                                                                                                                                                  module.exports = router;
+        console.log('Comment deleted successfully:', results);
+        res.redirect('/');
+      }
+    );
+  } catch (error) {
+    console.error('Error deleting comment:', error);
+    res.status(500).send('Error deleting comment');
+  }
+});
+
+/* PAGE ROUTES */
+router.get('/menu', (req, res) => {
+  res.render('menu');
+});
+
+router.get('/about_us', (req, res) => {
+  res.render('about_us');
+});
+
+router.get('/customer_comments', (req, res) => {
+  res.render('customer_comments');
+});
+
+router.get('/landing_page', (req, res) => {
+  res.render('landing_page', { title: 'Downtown Donuts' });
+});
+
+module.exports = router;
